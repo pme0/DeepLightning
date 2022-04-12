75d5c8780975a5f369c9041e1b4fd7e3ff267b9b
pme <12113751+pme0@users.noreply.github.com>
2022-04-12 13:35:26 +0100

    update test

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -4,30 +4,16 @@ sys.path.insert(0, "..")
 from omegaconf import OmegaConf
 import torch
 import pytorch_lightning as pl
-from pytorch_lightning.utilities import rank_zero_only
 import pytest
 
-from tests.helpers.tools import compare_model_params
-from tests.helpers.dummies import DummyModel
-from torch.utils.data import DataLoader, Dataset
-from tests.helpers.housekeeping import clear_temporary_dir
 from deeplightning.config.load import load_config
 from deeplightning.init.initializers import init_model, init_trainer
+from deeplightning.trainer.trainer import DLTrainer
+
 
 FILE_NAME = os.path.basename(__file__).split(".")[0]
 TMP_DIR = "tmp"
 CKPT_PATH = os.path.join(TMP_DIR, "last.ckpt")
-
-
-def test_initialization():
-
-    config = load_config(config_file = "helpers/dummy_config.yaml")
-    model = init_model(config)
-    trainer = init_trainer(config)
-
-    assert OmegaConf.is_config(config)
-    assert isinstance(model, pl.LightningModule)
-    assert isinstance(trainer, pl.Trainer)
 
 
 @pytest.mark.parametrize(
@@ -59,21 +45,27 @@ def test_initialization():
 def test_trainer(kwargs):
 
     config = load_config(config_file = "helpers/dummy_config.yaml")
+    
+    config.engine.backend = kwargs["strategy"]
+    config.engine.precision = kwargs["precision"]
+    config.engine.gpus = kwargs["gpus"]
+    # TODO extra params for quick testing
+    config.test_params.limit_train_batches = 2
+    config.test_params.limit_val_batches = 2
+    config.test_params.enable_model_summary = False,
+    config.test_params.enable_progress_bar = False,
+    config.test_params.logger = False
+    
     model = init_model(config)
-    trainer = pl.Trainer(
-        max_epochs = 1,
-        logger = False,
-        strategy = kwargs['strategy'], 
-        gpus = kwargs['gpus'],
-        precision = kwargs['precision'],
-        limit_train_batches = 2,
-        limit_val_batches = 2,
-        enable_model_summary = False,
-        enable_progress_bar = False,
-        enable_checkpointing = False,
-        )
+    trainer = init_trainer(config)
+
+    assert OmegaConf.is_config(config)
+    assert isinstance(model, pl.LightningModule)
+    assert isinstance(trainer, pl.Trainer)
+    assert isinstance(trainer, DLTrainer)
 
     trainer.fit(model)
 
     assert trainer.state.finished, f"Trainer state: {trainer.state}"
     assert trainer.current_epoch == (trainer.max_epochs-1)
+
