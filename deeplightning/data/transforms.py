b5e8067d7592ab6c9baf01e5a85fec3f9c35bb00
pme0 <pme0@>
2022-09-13 14:11:26 +0100

    .

--- deeplightning/data/transforms.py
+++ deeplightning/data/transforms.py
@@ -2,6 +2,49 @@ from torchvision import transforms as T
 import torchvision.transforms.functional as F
 import numpy as np
 
+from deeplightning.utilities.messages import info_message, warning_message
+
+
+def get_transforms(cfg, subset):
+    """
+    """
+    if cfg.data.transforms is None or cfg.data.transforms is False:
+        return None
+
+    DataTransforms = {
+    "affine": Affine,
+    "flip": HorizontalFlip,
+    "jitter": ColorJitter,
+    "normalize": Normalize,
+    "pad": Pad,
+    "padsquare": PadSquare,
+    "perspective": Perspective,
+    "resize": Resize,
+    "rotation": Rotation,
+    "totensor": ToTensor,
+    }
+
+    trfs = []
+    if cfg.data[subset] is None:
+        return trfs.append(DataTransforms["totensor"])
+    for k in cfg.data[subset].keys():
+        p = cfg.data[subset][k]
+        t = DataTransforms[k](p)
+        if t is not None:
+            if isinstance(t, list):
+                for x in t:
+                    trfs.append(x)
+            else:
+                trfs.append(t)
+        else:
+            warning_message(f"Transform '{k}' present in .cfg.data.{subset}' but unused due to unsuitable parameters ({cfg.data[subset][k]})")
+    
+    info_message("{}:".format(subset.upper()))
+    for i, k in enumerate(trfs):
+        info_message(f"  ({i+1}) {k}")
+
+    return T.Compose(trfs)
+
 
 def Affine(cfg):
     return T.RandomAffine(
@@ -58,17 +101,3 @@ def Rotation(cfg):
 
 def ToTensor(cfg):
     return T.ToTensor()
-
-
-DataTransforms = {
-    "affine": Affine,
-    "flip": HorizontalFlip,
-    "jitter": ColorJitter,
-    "normalize": Normalize,
-    "pad": Pad,
-    "padsquare": PadSquare,
-    "perspective": Perspective,
-    "resize": Resize,
-    "rotation": Rotation,
-    "totensor": ToTensor,
-}
