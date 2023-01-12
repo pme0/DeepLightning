8f55036d7ce96f30c06699860d70bb2229dd5e89
pme0 <12113751+pme0@users.noreply.github.com>
2023-01-12 04:09:29 +0000

    update viz - pedestrian_det

--- tools/pedestrian_detection.py
+++ tools/pedestrian_detection.py
@@ -1,7 +1,7 @@
 """ 
 Usage:
 
-    python  tools/pedestrian_detection.py  --model_cfg external/yolov5/models/yolov5x.yaml  --model_ckpt /Users/pme/code/yolov5/yolov5x6.pt  --input_path /Users/pme/Downloads/people.jpeg  --output_path /Users/pme/Downloads/tests/
+    python  tools/pedestrian_detection.py  --model_cfg external/yolov5/models/yolov5x.yaml  --model_ckpt /Users/pme/code/yolov5/yolov5x6.pt  --input_path /Users/pme/Downloads/pexels-kate-trifo-4019405.jpg  --output_path /Users/pme/Downloads/tests/
 
 """
 
@@ -47,7 +47,7 @@ class PedestrianDetector():
     """
     """
 
-    def __init__(self, model_cfg, model_ckpt, input_path, output_path):
+    def __init__(self, model_cfg, model_ckpt, input_path):
 
         if isinstance(input_path, str):
             if input_path.endswith(IMG_EXT):
@@ -61,11 +61,6 @@ class PedestrianDetector():
 
         self.model_cfg = model_cfg
         self.model_ckpt = model_ckpt
-        self.input_path = input_path
-        self.output_path = output_path
-        if not os.path.isdir(output_path):
-                os.makedirs(output_path)
-
         self.model = torch.hub.load(
             repo_or_dir = "external/yolov5/", 
             model = "custom", 
@@ -74,13 +69,13 @@ class PedestrianDetector():
             force_reload = True).eval()
         
 
-    def infer(self):
+    def infer(self, input_path):
         
         if self.input_type == "image":
 
-            w, h = imagesize.get(self.input_path)
-            image = Image.open(self.input_path)
-            transforms = T.Compose([T.Resize(640), T.ToTensor()])
+            w, h = imagesize.get(input_path)
+            image = Image.open(input_path)
+            transforms = T.Compose([T.Resize((640,640)), T.ToTensor()])
             image_tensors = transforms(image).unsqueeze(0)
 
         elif self.input_type == "video":
@@ -92,7 +87,7 @@ class PedestrianDetector():
             raise NotImplementedError
 
 
-        conf_thres=0.25     # confidence threshold
+        conf_thres=0.30     # confidence threshold
         iou_thres=0.45      # IoU threshold
         classes = 0         # class filter
         max_det = 1000      # maximum number of detections
@@ -100,7 +95,11 @@ class PedestrianDetector():
 
         pred = self.model(image_tensors)
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
-        pred = pred * w / 640
+        # rescale bboxes to original image size
+        pred[:,0] = pred[:,0] * w / 640
+        pred[:,1] = pred[:,1] * h / 640
+        pred[:,2] = pred[:,2] * w / 640
+        pred[:,3] = pred[:,3] * h / 640
 
         self.bboxes = []
         for obj in pred:
@@ -111,17 +110,19 @@ class PedestrianDetector():
                 "conf": float(obj[4]), 
             })
 
- 
-    def vizualize(self):
+
+    def vizualize(self, input_path, output_path, resize, show_label, blur_bboxes):
         """
         """
         if self.input_type == "image":
-
             self.plot_image_and_bboxes(
-                image_path = self.input_path, 
-                resize = None, 
-                output_path = self.output_path, 
-            )
+                image_path = input_path, 
+                output_path = output_path,
+                resize = resize, 
+                show_label = show_label,
+                blur_bboxes = blur_bboxes)
+        else:
+            raise NotImplementedError
 
 
     def plot_image_and_bboxes(
@@ -129,6 +130,8 @@ class PedestrianDetector():
         image_path: str, 
         resize: int = None, 
         output_path: str = None, 
+        show_label: bool = True,
+        blur_bboxes: bool = False,
         ):
         """Plot an image together with a set of bounding boxes and class labels
 
@@ -149,28 +152,31 @@ class PedestrianDetector():
         
         image = Image.open(image_path)
         img_w, img_h = imagesize.get(image_path)
-        assert img_w == img_h  # assumes square image
-        
-        if resize is None:
-            resize = img_w
-        else:
-            image = image.resize((resize, resize))
-        
-            
+
         # params
-        font_factor = 0.025
-        label_width_factor = 0.00005 * img_h
-        label_heigth_factor = 0.00011 * img_h
+        label_linewidth = 0.02 * img_w
+        label_fontsize = 0.018 * img_w
+        label_width_factor = 0.8 * label_fontsize
+        label_heigth_factor = 1.4 * label_fontsize
         
         # plot
         # figure size is set in pixels; by default some pixels in each 
         # direction are allocated to white border/axes, so adjustment for that 
         # is required if the saved image is to be exactly `resize x resize`;
-        resize_extra = 1.3 * resize
+        resize_extra = 1.3 * img_w
         # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/figure_size_units.html#figure-size-in-pixel
         px = 1/plt.rcParams['figure.dpi']
         fig, ax = plt.subplots(1,1, figsize=(resize_extra*px, resize_extra*px))  
         
+        # blur bboxes
+        if blur_bboxes:
+            for i, bbox in enumerate(self.bboxes):
+                cbox = (math.floor(bbox["box"][0])-1, math.floor(bbox["box"][1])-1, math.ceil(bbox["box"][0]+bbox["box"][2])+1, math.floor(bbox["box"][1]+bbox["box"][3])+1) #left, upper, right, and lower
+                ic = image.crop(cbox)
+                for i in range(3):  # use BLUR filter multiple times to get the desired effect
+                    ic = ic.filter(ImageFilter.BLUR)
+                image.paste(ic, cbox)
+
         plt.imshow(image)
             
         for i, bbox in enumerate(self.bboxes):
@@ -189,41 +195,43 @@ class PedestrianDetector():
             )
             
             # label box
-            label_conf = None if "conf" not in bbox else bbox["conf"]
-            label_text = "{}{}{:.2f}".format(OBJECTS[bbox["class"]].lower(), "" if label_conf is None else " ", round(label_conf,2))
-            label_width = label_width_factor * resize_extra * len(label_text)
-            label_heigth = label_heigth_factor * resize_extra
-            ax.add_patch(
-                patches.Rectangle(
-                    xy=(bbox["box"][0], bbox["box"][1]+bbox["box"][3] - label_heigth), 
-                    width=label_width, 
-                    height=label_heigth, 
-                    facecolor=colors[i%len(colors)], #colors[box["class"]]
-                    edgecolor='none',
-                    linewidth=2,
-                    alpha=0.6,
+            if show_label:
+                label_conf = None if "conf" not in bbox else bbox["conf"]
+                label_text = "{}{}{:.2f}".format(OBJECTS[bbox["class"]].lower(), "" if label_conf is None else " ", round(label_conf,2))
+                label_width = label_width_factor * len(label_text)
+                label_heigth = label_heigth_factor
+                ax.add_patch(
+                    patches.Rectangle(
+                        xy=(bbox["box"][0], bbox["box"][1]+bbox["box"][3] - label_heigth), 
+                        width=label_width, 
+                        height=label_heigth, 
+                        facecolor=colors[i%len(colors)], #colors[box["class"]]
+                        edgecolor='none',
+                        linewidth=label_linewidth,
+                        alpha=0.6,
+                    )
                 )
-            )
 
             # label text
-            fontsize = font_factor * img_h
-            plt.annotate(
-                text=label_text, 
-                xy=(bbox["box"][0], bbox["box"][1]+bbox["box"][3]),
-                color='white',
-                ha="left",
-                va = "bottom",
-                alpha=1,
-                font=dict(size=fontsize),
-                clip_on=True, # avoids annotation outside the plot area, which cretes a white margin that cannot be trimmed by off-ing the axes
-            )
+            if show_label:
+                plt.annotate(
+                    text=label_text, 
+                    xy=(bbox["box"][0], bbox["box"][1]+bbox["box"][3]),
+                    color='white',
+                    ha="left",
+                    va = "bottom",
+                    alpha=1,
+                    font=dict(size=label_fontsize),
+                    clip_on=True, # avoids annotation outside the plot area, which cretes a white margin that cannot be trimmed by off-ing the axes
+                )
         # https://stackoverflow.com/questions/11837979/removing-white-space-around-a-saved-image
         plt.gca().set_axis_off()
 
         if output_path is not None:
+            if not os.path.isdir(output_path):
+                os.makedirs(output_path)
             plt.savefig(os.path.join(output_path, "test.png"), bbox_inches='tight', pad_inches=0)
         plt.close()
-
 
 
 if __name__ == "__main__":
@@ -250,13 +258,16 @@ if __name__ == "__main__":
 
     detector = PedestrianDetector(
         model_cfg = args.model_cfg, 
-        model_ckpt = args.model_ckpt, 
-        input_path = args.input_path,
+        model_ckpt = args.model_ckpt,
+        input_path = args.input_path)
+
+    detector.infer(
+        input_path = args.input_path)
+
+    detector.vizualize(
+        input_path = args.input_path, 
         output_path = args.output_path,
-    )
-
-    detector.infer()
-
-    detector.vizualize()
-
+        resize = None,
+        show_label = True,
+        blur_bboxes = True)
     
