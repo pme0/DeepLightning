76e65fe96db774587076204942e0b692e64593cb
pme0 <>
2023-01-12 01:47:45 +0000

    .

--- infer.py
+++ infer.py
@@ -1,6 +1,6 @@
+import os
 import sys
 import argparse
-import wandb
 
 from deeplightning.utils.messages import info_message, warning_message, error_message
 from deeplightning.config.load import load_config
@@ -19,4 +19,6 @@ if __name__ == "__main__":
     args = parse_command_line_arguments()
     cfg = load_config(config_file = args.cfg)
 
-    
+    os.system("python deeplightning/inference/object_detection.py --input_path {} --model_cfg {} --model_ckpt {}".format(
+        cfg.input_path, cfg.model_cfg, cfg.model_ckpt,
+    ))
