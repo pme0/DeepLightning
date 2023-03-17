f7d7a763035d46ce2594e2bb0dbb2f2fed9d9136
pme0 <pme@192.168.1.117>
2023-03-17 17:06:43 +0000

    hooks update

--- deeplightning/trainer/hooks/ImageClassification_hooks.py
+++ deeplightning/trainer/hooks/ImageClassification_hooks.py
@@ -13,7 +13,7 @@ def training_step__ImageClassification(self, batch, batch_idx):
     Parameters
     ----------
     batch : object containing the data output by the dataloader. For custom 
-        datasets this is a dictionary with keys ["paths", "images", "labels"].
+        datasets this is a dictionary with keys ["paths", "inputs", "labels"].
         For torchvision datasets, the function `dictionarify_batch()` is used
         to convert the native format to dictionary format
     batch_idx : index of batch
@@ -47,6 +47,7 @@ def training_step_end__ImageClassification(self, training_step_outputs):
         dictionary in single-device training, or list of 
         metrics dictionaries in multi-device training (one 
         element per device). The output from `training_step()`.
+        
     """
      
     if self.global_step % self.cfg.logger.log_every_n_steps == 0:
@@ -82,10 +83,7 @@ def training_epoch_end__ImageClassification(self, training_step_outputs):
         dictionary in single-device training, or list of 
         metrics dictionaries in multi-device training (one 
         element per device). The output from `training_step()`.
-    logger :
-    global_step : 
-    step_label : 
-    log_to_wandb : 
+
     """
 
     # log training metrics on the last batch only
@@ -97,7 +95,7 @@ def training_epoch_end__ImageClassification(self, training_step_outputs):
 
 
 def validation_step__ImageClassification(self, batch, batch_idx):
-    """ Hook for validation step.
+    """ Hook for `validation_step`.
 
     Parameters
     ----------
@@ -106,6 +104,7 @@ def validation_step__ImageClassification(self, batch, batch_idx):
         For torchvision datasets, the function `dictionarify_batch()` is used
         to convert the native format to dictionary format
     batch_idx : index of batch
+
     """
 
     # convert batch to dictionary form
@@ -120,8 +119,8 @@ def validation_step__ImageClassification(self, batch, batch_idx):
 
     # metrics
     self.metrics["Accuracy_val"].update(preds = preds, target = batch["labels"])
-    self.metrics["ConfusionMatrix"].update(preds = preds, target = batch["labels"])
-    self.metrics["PrecisionRecallCurve"].update(preds = outputs, target = batch["labels"])
+    self.metrics["ConfusionMatrix_val"].update(preds = preds, target = batch["labels"])
+    self.metrics["PrecisionRecallCurve_val"].update(preds = outputs, target = batch["labels"])
         
     return {
         "val_loss": loss, 
@@ -130,7 +129,7 @@ def validation_step__ImageClassification(self, batch, batch_idx):
 
 
 def validation_step_end__ImageClassification(self, validation_step_outputs):
-    """ Hook for validation step_end.
+    """ Hook for `validation_step_end`.
 
     Parameters
     ----------
@@ -152,7 +151,7 @@ def validation_step_end__ImageClassification(self, validation_step_outputs):
 
 
 def validation_epoch_end__ImageClassification(self, validation_epoch_outputs):
-    """ Hook for validation epoch_end.
+    """ Hook for `validation_epoch_end`.
 
     Parameters
     ----------
@@ -175,16 +174,16 @@ def validation_epoch_end__ImageClassification(self, validation_epoch_outputs):
     self.metrics["Accuracy_val"].reset()
 
     # confusion matrix
-    cm = self.metrics["ConfusionMatrix"].compute()
-    figure = self.metrics["ConfusionMatrix"].draw(cm, subset="val", epoch=self.current_epoch+1)
+    cm = self.metrics["ConfusionMatrix_val"].compute()
+    figure = self.metrics["ConfusionMatrix_val"].draw(cm, subset="val", epoch=self.current_epoch+1)
     metrics["val_confusion_matrix"] = wandb.Image(figure, caption=f"Confusion Matrix [val, epoch {self.current_epoch+1}]")
-    self.metrics["ConfusionMatrix"].reset()
+    self.metrics["ConfusionMatrix_val"].reset()
         
     # precision-recall
-    precision, recall, thresholds = self.metrics["PrecisionRecallCurve"].compute()
-    figure = self.metrics["PrecisionRecallCurve"].draw(precision=precision, recall=recall, thresholds=thresholds, subset="val", epoch=self.current_epoch+1)
+    precision, recall, thresholds = self.metrics["PrecisionRecallCurve_val"].compute()
+    figure = self.metrics["PrecisionRecallCurve_val"].draw(precision=precision, recall=recall, thresholds=thresholds, subset="val", epoch=self.current_epoch+1)
     metrics["val_precision_recall"] = wandb.Image(figure, caption=f"Precision-Recall Curve [val, epoch {self.current_epoch+1}]")
-    self.metrics["PrecisionRecallCurve"].reset()
+    self.metrics["PrecisionRecallCurve_val"].reset()
 
     # log validation metrics
     if self.cfg.logger.log_to_wandb:
@@ -199,3 +198,96 @@ def validation_epoch_end__ImageClassification(self, validation_epoch_outputs):
     if self.cfg.train.early_stop_metric is not None:
         m = self.cfg.train.early_stop_metric
         self.log(m, metrics[m])
+
+
+def test_step__ImageClassification(self, batch, batch_idx):
+        """ Hook for `test_step`.
+
+        Parameters
+        ----------
+        batch : object containing the data output by the dataloader. For custom 
+            datasets this is a dictionary with keys ["paths", "images", "labels"].
+            For torchvision datasets, the function `dictionarify_batch()` is used
+            to convert the native format to dictionary format
+        batch_idx: index of batch.
+        
+        """
+
+        # convert batch to dictionary form
+        batch = dictionarify_batch(batch, self.cfg.data.dataset)
+
+        # forward pass
+        outputs = self.model(batch["inputs"])
+        preds = torch.argmax(outputs, dim=1)
+            
+        # loss
+        loss = self.loss(outputs, batch["labels"])
+
+        # metrics
+        self.metrics["Accuracy_test"].update(preds = preds, target = batch["labels"])
+        self.metrics["ConfusionMatrix_test"].update(preds = preds, target = batch["labels"])
+        self.metrics["PrecisionRecallCurve_test"].update(preds = outputs, target = batch["labels"])
+            
+        return {"test_loss": loss}
+
+
+def test_step_end__ImageClassification(self, test_step_outputs):
+    """ Hook for `test_step_end`.
+
+    Parameters
+    ----------
+    test_step_outputs : (dict, list[dict]) metrics 
+        dictionary in single-device training, or list of 
+        metrics dictionaries in multi-device training (one 
+        element per device). The output from `test_step()`.
+
+    """
+
+    # aggregate metrics across all devices.
+    metrics = self.gather_on_step(
+        step_outputs = test_step_outputs, 
+        metrics = ["test_loss"], 
+        average = False)
+
+    return metrics
+
+
+def test_epoch_end__ImageClassification(self, test_epoch_outputs):
+    """ Hook for `test_epoch_end`.
+
+    Parameters
+    ----------
+    test_epoch_outputs : (dict, list[dict]) metrics 
+        dictionary in single-device training, or list of 
+        metrics dictionaries in multi-device training (one 
+        element per device). 
+        The output from `test_step_end()`.
+
+    """
+
+    # aggregate losses across all steps and average
+    metrics = self.gather_on_epoch(
+        epoch_outputs = test_epoch_outputs, 
+        metrics = ["test_loss"], 
+        average = True)
+
+    # accuracy
+    metrics["test_acc"] = self.metrics["Accuracy_test"].compute()
+    self.metrics["Accuracy_test"].reset()
+
+    # confusion matrix
+    cm = self.metrics["ConfusionMatrix_test"].compute()
+    figure = self.metrics["ConfusionMatrix_test"].draw(cm, subset="test", epoch=self.current_epoch+1)
+    metrics["test_confusion_matrix"] = wandb.Image(figure, caption=f"Confusion Matrix [test, epoch {self.current_epoch+1}]")
+    self.metrics["ConfusionMatrix_test"].reset()
+        
+    # precision-recall
+    precision, recall, thresholds = self.metrics["PrecisionRecallCurve_test"].compute()
+    figure = self.metrics["PrecisionRecallCurve_test"].draw(precision=precision, recall=recall, thresholds=thresholds, subset="test", epoch=self.current_epoch+1)
+    metrics["test_precision_recall"] = wandb.Image(figure, caption=f"Precision-Recall Curve [test, epoch {self.current_epoch+1}]")
+    self.metrics["PrecisionRecallCurve_test"].reset()
+
+    # log test metrics
+    if self.cfg.logger.log_to_wandb:
+        metrics[self.step_label] = self.global_step
+        wandb.log(metrics)
