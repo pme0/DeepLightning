1ea8511cd2659f94a9e09d393e7d26adb7e47d4d
pme0 <pme0@>
2023-03-19 22:39:58 +0000

    .

--- deeplightning/init/initializers.py
+++ deeplightning/init/initializers.py
@@ -6,7 +6,9 @@ from pytorch_lightning import LightningModule, LightningDataModule
 from deeplightning.config.defaults import __ConfigGroups__
 from deeplightning.trainer.trainer import DLTrainer
 from deeplightning.init.imports import init_module
-from deeplightning.utils.registry import __MetricsRegistry__
+from deeplightning.utils.registry import (__MetricsRegistry__, 
+                                          __LoggerRegistry__, 
+                                          __HooksRegistry__)
 
 
 
@@ -39,14 +41,32 @@ def init_trainer(cfg: OmegaConf) -> DLTrainer:
     return DLTrainer(cfg, args)
 
 
+def init_logger(cfg: OmegaConf) -> None:
+        """ Initialize Logger
+        """
+
+        # load logger
+        logger = __LoggerRegistry__[cfg.logger.name](
+            cfg = cfg,
+            logged_metric_names = __HooksRegistry__[cfg.task]["LOGGED_METRICS_NAMES"]
+        )
+
+        # ensure all required attributes have been initialised
+        attributes = ["run_id", "run_name", "run_dir", "artifact_path"]
+        for attribute in attributes:
+            if not hasattr(logger, attribute):
+                raise AttributeError(f"Attribute '{attribute}' has not been set in DLLoger")
+            
+        return logger
+
+
 def init_everything(cfg: OmegaConf) -> Tuple[LightningModule, LightningDataModule, DLTrainer]:
     """ Initialize main modules
     """
     model = init_model(cfg)
     data = init_dataset(cfg)
     trainer = init_trainer(cfg)
-    cfg = trainer.cfg
-    return cfg, model, data, trainer
+    return model, data, trainer
 
 
 def init_metrics(cfg: OmegaConf) -> dict:
--- deeplightning/logger/wandb.py
+++ deeplightning/logger/wandb.py
@@ -1,17 +1,27 @@
+from typing import Any, Union, Tuple, Optional, List
 from omegaconf import OmegaConf
 import wandb
 
 from deeplightning.logger.helpers import add_logger_params_to_config
 
 
+
 class wandbLogger():
     """ Deep Lightning Logger.
     """
-    def __init__(self, cfg: OmegaConf) -> None:
+    def __init__(self, cfg: OmegaConf, logged_metric_names: List[str]) -> None:
         self.cfg = cfg
-        self.init_wandb()
+        self.init_wandb_server()
+        self.step_label = "iteration"
+        self.init_wandb_metrics(
+            metric_names = logged_metric_names,
+            step_label = self.step_label,
+        )
 
-    def init_wandb(self):
+
+    def init_wandb_server(self):
+        """ Initialise W&B sever
+        """
         wandb.init(
             project = self.cfg.logger.project_name,
             notes = self.cfg.logger.notes,
@@ -25,8 +35,30 @@ class wandbLogger():
         self.artifact_path = wandb.run.dir
 
         # add logger params to config - so that it can be stored with the runtime parameters
-        self.cfg = add_logger_params_to_config(self.cfg)
+        self.cfg = add_logger_params_to_config(
+            cfg = self.cfg,
+            run_id = self.run_id,
+            run_name = self.run_name,
+            run_dir = self.run_dir,
+            artifact_path = self.artifact_path,
+        )
 
 
-    def log_image(self):
-        pass
+    def init_wandb_metrics(self, metric_names: list, step_label: str) -> None:
+        """ Define a custom x-axis metric `step_label` which is 
+            synchronised with PyTprch-Lightning's `global_step`; and
+            defines all other `metrics` to be plotted agains `step_label`
+        """
+
+        # define custom x-axis metric `step_label` (synchronised with PL `global_step`)
+        wandb.define_metric(step_label)
+        
+        # initialise metrics to be plotted against `step_label`
+        for m in metric_names:
+            wandb.define_metric(m, step_metric=step_label)
+
+        return step_label
+    
+
+    def log_metrics(self, metrics: dict) -> None:
+        wandb.log(metrics)
