a2930403af5fb06db8726eac7aabc3ab422f5bbf
pme0 <12113751+pme0@users.noreply.github.com>
2023-07-01 21:11:50 +0100

    update UNet

--- deeplightning/model/unet.py
+++ deeplightning/model/unet.py
@@ -3,37 +3,76 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ConvBlock(nn.Module):
+class DoubleConv(nn.Module):
+    """downsampling with two conv3x3-bn-relu blocks"""
     def __init__(self, in_channels, out_channels, use_batchnorm=False):
-        super(ConvBlock, self).__init__()
-        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True)]
+        super(DoubleConv, self).__init__()
+        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0)]
         layers += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
         layers += [nn.ReLU()]
-        layers += [nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True)]
+        layers += [nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=0)]
         layers += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
         layers += [nn.ReLU()]
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.layers(x)
+    
+
+class DownSampleConvBlock(nn.Module):
+    """downsample with maxpool followed by doubleconv block"""
+    def __init__(self, in_channels, out_channels, use_batchnorm=False):
+        super(DownSampleConvBlock, self).__init__()
+        self.layers = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+        
+    def forward(self, x):
+        return self.layers(x)
 
 
+class UpConv(nn.Module):
+    """upsampling with conv2x2-relu block"""
+    def __init__(self, in_channels, out_channels, use_batchnorm=False):
+        super(UpConv, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=2, stride=1, padding=0, bias=True),
+            nn.ReLU()
+        )
+        
+    def forward(self, x):
+        return self.layers(x)
+
+    
 class UpsampleConvBlock(nn.Module):
+    """upsampling with upsample-upconv-doubleconv block"""
     def __init__(self, in_channels, out_channels, use_batchnorm=False):
         super(UpsampleConvBlock, self).__init__()
         self.upsample = nn.Upsample(scale_factor=2)
-        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=2, stride=1, padding=0, bias=True)]
-        layers += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
-        layers += [nn.ReLU()]
-        self.layers = nn.Sequential(*layers)
+        self.upconv = UpConv(in_channels, out_channels)
+        self.conv = DoubleConv(in_channels, out_channels)
+        
+    def crop(self, source, target):
+        """crop source tensor to target tensor's shape"""
+        _, _, w_old, h_old = source.size()  # get the original width and height
+        _, _, w_new, h_new = target.size()  # get the original width and height
+        left = (w_old - w_new) // 2  # calculate the left offset for cropping
+        top = (h_old - h_new) // 2  # calculate the top offset for cropping
+        return source[:, :, left:left+w_new, top:top+h_new]
 
-    def forward(self, x):
+    def forward(self, x, y):
+        """ x : the input feature map from the expansive path
+            y : the corresponding feature map from the contracting path
+        """
         x = self.upsample(x)
         # The following padding is not clear from the paper how it should be 
         # implemented but it is necessary in order to get to right feature map 
         # shapes. Should we pad from right or left, top or bottom?
         x = F.pad(x, (0, 1, 0, 1))
-        x = self.layers(x)
+        x = self.upconv(x)
+        x = torch.cat((self.crop(y, x), x), dim=1)
+        x = self.conv(x)
         return x
 
 
@@ -57,56 +96,32 @@ class UNet(nn.Module):
     References
     ----------
     'U-Net: Convolutional Networks for Biomedical Image Segmentation', https://arxiv.org/abs/1505.04597
-        
+    
     """
     def __init__(self, in_channels, out_channels, use_batchnorm=False):
         super(UNet, self).__init__()
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv1 = ConvBlock(in_channels, 64, use_batchnorm)
-        self.conv2 = ConvBlock(64, 128, use_batchnorm)
-        self.conv3 = ConvBlock(128, 256, use_batchnorm)
-        self.conv4 = ConvBlock(256, 512, use_batchnorm)
-        self.conv5 = ConvBlock(512, 1024, use_batchnorm)
-        self.upsample_conv_5 = UpsampleConvBlock(1024, 512, use_batchnorm)
-        self.up_conv5 = ConvBlock(1024, 512, use_batchnorm)
-        self.upsample_conv_4 = UpsampleConvBlock(512, 256, use_batchnorm)
-        self.up_conv4 = ConvBlock(512, 256, use_batchnorm)
-        self.upsample_conv_3 = UpsampleConvBlock(256, 128, use_batchnorm)
-        self.up_conv3 = ConvBlock(256, 128, use_batchnorm)
-        self.upsample_conv_2 = UpsampleConvBlock(128, 64, use_batchnorm)
-        self.up_conv2 = ConvBlock(128, 64, use_batchnorm)
+        self.first_conv = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=0)
+        self.down1 = DownSampleConvBlock(64, 128, use_batchnorm)
+        self.down2 = DownSampleConvBlock(128, 256, use_batchnorm)
+        self.down3 = DownSampleConvBlock(256, 512, use_batchnorm)
+        self.down4 = DownSampleConvBlock(512, 1024, use_batchnorm)
+        self.up4 = UpsampleConvBlock(1024, 512, use_batchnorm)
+        self.up3 = UpsampleConvBlock(512, 256, use_batchnorm)
+        self.up2 = UpsampleConvBlock(256, 128, use_batchnorm)
+        self.up1 = UpsampleConvBlock(128, 64, use_batchnorm)
         self.last_conv = nn.Conv2d(64, out_channels, kernel_size=1, stride=1, padding=0)
         self.activation = torch.nn.Sigmoid()
 
-    def crop(self, source, target):
-        _, _, w_old, h_old = source.size()  # get the original width and height
-        _, _, w_new, h_new = target.size()  # get the original width and height
-        left = (w_old - w_new) // 2  # calculate the left offset for cropping
-        top = (h_old - h_new) // 2  # calculate the top offset for cropping
-        return source[:, :, left:left+w_new, top:top+h_new]
-
     def forward(self, x):
-        e1 = self.conv1(x)
-        e2 = self.maxpool(e1)
-        e2 = self.conv2(e2)
-        e3 = self.maxpool(e2)
-        e3 = self.conv3(e3)
-        e4 = self.maxpool(e3)
-        e4 = self.conv4(e4)
-        e5 = self.maxpool(e4)
-        x = self.conv5(e5)
-        x = self.upsample_conv_5(x)
-        x = torch.cat((self.crop(e4, x), x), dim=1)
-        x = self.up_conv5(x)
-        x = self.upsample_conv_4(x)
-        x = torch.cat((self.crop(e3, x), x), dim=1)
-        x = self.up_conv4(x)
-        x = self.upsample_conv_3(x)
-        x = torch.cat((self.crop(e2, x), x), dim=1)
-        x = self.up_conv3(x)
-        x = self.upsample_conv_2(x)
-        x = torch.cat((self.crop(e1, x), x), dim=1)
-        x = self.up_conv2(x)
+        x1 = self.first_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x = self.down4(x4)
+        x = self.up4(x, x4)
+        x = self.up3(x, x3)
+        x = self.up2(x, x2)
+        x = self.up1(x, x1)
         x = self.last_conv(x)
         x = self.activation(x)
         return x
