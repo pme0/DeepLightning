ae3d68205e36479f447916f6028861ae994bc615
pme0 <12113751+pme0@users.noreply.github.com>
2023-07-01 21:16:17 +0100

    update UNet

--- deeplightning/model/unet.py
+++ deeplightning/model/unet.py
@@ -7,12 +7,15 @@ class DoubleConv(nn.Module):
     """downsampling with two conv3x3-bn-relu blocks"""
     def __init__(self, in_channels, out_channels, use_batchnorm=False):
         super(DoubleConv, self).__init__()
-        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0)]
-        layers += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
-        layers += [nn.ReLU()]
-        layers += [nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=0)]
-        layers += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
-        layers += [nn.ReLU()]
+        layers = []
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0))
+        if use_batchnorm:
+            layers.append(nn.BatchNorm2d(out_channels))
+        layers.append(nn.ReLU())
+        layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=0))
+        if use_batchnorm:
+            layers.append(nn.BatchNorm2d(out_channels))
+        layers.append(nn.ReLU())
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
