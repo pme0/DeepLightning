8a34bca2157bd14718c963b7ec8be3f0e65defa1
pme0 <12113751+pme0@users.noreply.github.com>
2023-11-05 21:16:53 +0000

    cleanup

--- deeplightning/task/vision/segmentation.py
+++ deeplightning/task/vision/segmentation.py
@@ -36,57 +36,24 @@ class SemanticSegmentationTask(BaseTask):
     """
     def __init__(self, cfg: OmegaConf):
         super().__init__(cfg=cfg)
+        
         self.loss = init_obj_from_config(cfg.model.loss)
         self.model = init_obj_from_config(cfg.model.network)
         self.optimizer = init_obj_from_config(cfg.model.optimizer, self.model.parameters())
         self.scheduler = init_obj_from_config(cfg.model.scheduler, self.optimizer)
         
-        # migration from `pytorch_lightning==1.5.10` to `lightning==2.0.0`
         self.training_step_outputs = {"train_loss": []}
         self.validation_step_outputs = {"val_loss": []}
         self.test_step_outputs = {"test_loss": []}
 
-        # PyTorch-Lightning performs a partial validation epoch to ensure that
-        # everything is correct. Use this to avoid logging metrics to W&B for that 
-        #self.sanity_check = True
-
-        # Initialise metrics to track during training
-        torch_device = torch.device("cuda") if cfg.engine.accelerator == "gpu" else torch.device('cpu')
-
-        #self.metrics = init_metrics(cfg, device=torch_device)
         self.metrics = {
             "Accuracy_train": classification_accuracy(cfg),
             "Accuracy_val": classification_accuracy(cfg),
             "Accuracy_test": classification_accuracy(cfg),
         }
 
-        # Initialise label to track metrics against
-        self.step_label = "iteration"
+        self.on_task_init_end()
 
-        # Define hook functions
-        # to make the hooks bound to the class (so that they can access class attributes 
-        #  using `self.something`), the assignment must specify the class name as follows:
-        # `ClassName.fn = my_fn` rather than `self.fn = my_fn`
-        #SemanticSegmentationTask._training_step = __HooksRegistry__[cfg.task]["training_step"]
-        #SemanticSegmentationTask._training_step_end = __HooksRegistry__[cfg.task]["training_step_end"]
-        #SemanticSegmentationTask._on_training_epoch_end = __HooksRegistry__[cfg.task]["on_training_epoch_end"]
-        #SemanticSegmentationTask._validation_step = __HooksRegistry__[cfg.task]["validation_step"]
-        #SemanticSegmentationTask._validation_step_end = __HooksRegistry__[cfg.task]["validation_step_end"]
-        #SemanticSegmentationTask._on_validation_epoch_end = __HooksRegistry__[cfg.task]["on_validation_epoch_end"]
-        #SemanticSegmentationTask._test_step = __HooksRegistry__[cfg.task]["test_step"]
-        #SemanticSegmentationTask._test_step_end = __HooksRegistry__[cfg.task]["test_step_end"]
-        #SemanticSegmentationTask._on_test_epoch_end = __HooksRegistry__[cfg.task]["on_test_epoch_end"]
-
-        # Aggregation utilities
-        #self.gather_on_step = gather_on_step
-        #self.gather_on_epoch = gather_on_epoch
-
-        # PyTorch-Lightning's model summary does not give the 
-        # correct  number of trainable parameters; see 
-        # https://github.com/PyTorchLightning/pytorch-lightning/issues/12130
-        self.trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
-        info_message("Trainable parameters: {:,d}".format(self.trainable_params))
-       
 
     def forward(self, x: Tensor) -> Tensor:
         """ Model forward pass.
